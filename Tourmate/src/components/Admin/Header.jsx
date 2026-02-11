4ea92996730af90369d6775b9d4e930c2af5f17a
Ryuklyn <ryukace111@gmail.com>
2026-02-11 07:48:54 +0545

    admin page optimization

--- Tourmate/src/components/Admin/Header.jsx
+++ Tourmate/src/components/Admin/Header.jsx
@@ -1,4 +1,4 @@
-import { Bell, ChevronDown } from "lucide-react";
+import { Bell, Search } from "lucide-react";
 import { useState } from "react";
 import NotificationDropdown from "./NotificationDropdown";
 
@@ -7,11 +7,18 @@ export default function Header() {
 
   return (
     <header className="h-16 bg-white border border-gray-200 flex items-center justify-between px-6 sticky top-0 z-50">
-      <input
-        type="text"
-        placeholder="🔍  Search anything..."
-        className="w-96 px-4 py-2 rounded-lg bg-gray-100 outline-none"
-      />
+      <div className="relative">
+        <Search
+          size={18}
+          className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+        />
+
+        <input
+          type="text"
+          placeholder="Search anything..."
+          className="w-96 pl-10 pr-4 py-2 rounded-lg bg-gray-100 outline-none focus:ring-2 focus:ring-orange-400"
+        />
+      </div>
 
       <div className="flex items-center gap-6 relative">
         {/* Bell */}
@@ -34,7 +41,6 @@ export default function Header() {
             <p className="font-semibold">Super Admin</p>
             <p className="text-xs text-gray-500">admin@tourmate.com</p>
           </div>
-          <ChevronDown size={18} />
         </div>
       </div>
     </header>
